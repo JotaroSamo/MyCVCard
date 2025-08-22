aeb08f2bc265e3d819c2d9658fb72c2c2f4f7746
a.samoshuk <a.samoshuk@rit.va>
2025-08-22 13:04:42 +0300

    -fix

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,4 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { IntroComponent } from './components/intro/intro.component';
 import { AboutComponent } from './components/about/about.component';
@@ -10,7 +9,7 @@ import { ContactsComponent } from './components/contacts/contacts.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, IntroComponent, AboutComponent, ExperienceComponent, EducationSkillsComponent, SkillsComponent, ContactsComponent],
+  imports: [HeaderComponent, IntroComponent, AboutComponent, ExperienceComponent, EducationSkillsComponent, SkillsComponent, ContactsComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterLink } from '@angular/router';
 import { TranslatePipe } from '../../pipes/translate.pipe';
 import { I18nService } from '../../services/i18n.service';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, RouterLink, TranslatePipe],
+  imports: [CommonModule, TranslatePipe],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
